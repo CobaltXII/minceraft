da770c08fbaeb491d20735999d887586ffeb2464
CobaltXII <sidatt64@gmail.com>
2018-12-28 16:45:37 -0500

    Added image headers

---
+++ src/inc/image.hpp
@@ -0,0 +1,159 @@
+#include <vector>
+#include <string>
+
+// A list of all the files in /tex (excluding file extensions).
+
+std::vector<std::string> all_tex =
+{
+	"bedrock",
+
+	"birch_leaves",
+
+	"birch_log_side",
+
+	"birch_log_top",
+
+	"birch_planks",
+
+	"bookshelf",
+
+	"birck",
+
+	"coal_ore",
+
+	"cobblestone",
+
+	"diamond_ore",
+
+	"dirt",
+
+	"emerald_ore",
+
+	"gold_ore",
+
+	"grass_side",
+
+	"grass_top",
+
+	"gravel",
+
+	"iron_ore",
+
+	"mushroom_block_brown",
+
+	"mushroom_block_red",
+
+	"mushroom_block_stem_side",
+
+	"mushroom_block_stem_top",
+
+	"mycelium_side",
+
+	"mycelium_top",
+
+	"oak_leaves",
+
+	"oak_log_side",
+
+	"oak_planks",
+
+	"sand",
+
+	"stone",
+
+	"water_0",
+
+	"water_1",
+
+	"water_10",
+
+	"water_11",
+
+	"water_12",
+
+	"water_13",
+
+	"water_14",
+
+	"water_15",
+
+	"water_16",
+
+	"water_17",
+
+	"water_18",
+
+	"water_19",
+
+	"water_2",
+
+	"water_20",
+
+	"water_21",
+
+	"water_22",
+
+	"water_23",
+
+	"water_24",
+
+	"water_25",
+
+	"water_26",
+
+	"water_27",
+
+	"water_28",
+
+	"water_29",
+
+	"water_3",
+
+	"water_30",
+
+	"water_31",
+
+	"water_4",
+
+	"water_5",
+
+	"water_6",
+
+	"water_7",
+
+	"water_8",
+
+	"water_9",
+
+	"wool_colored_black",
+
+	"wool_colored_blue",
+
+	"wool_colored_brown",
+
+	"wool_colored_cyan",
+
+	"wool_colored_gray",
+
+	"wool_colored_green",
+
+	"wool_colored_light_blue",
+
+	"wool_colored_lime",
+
+	"wool_colored_magenta",
+
+	"wool_colored_orange",
+
+	"wool_colored_pink",
+
+	"wool_colored_purple",
+
+	"wool_colored_red",
+
+	"wool_colored_silver",
+
+	"wool_colored_white",
+
+	"wool_colored_yellow"
+};
