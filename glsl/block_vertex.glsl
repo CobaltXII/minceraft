e5e147e327a7ad1b4d685e62694728baa6110aa6
CobaltXII <sidatt64@gmail.com>
2018-12-28 20:53:27 -0500

    glsl/block_vertex.glsl now accepts all vertex attributes

--- glsl/block_vertex.glsl
+++ glsl/block_vertex.glsl
@@ -2,6 +2,9 @@
 
 layout (location = 0) in vec3 vertex_position;
 
+layout (location = 1) in vec3 vertex_texture;
+
+layout (location = 2) in float vertex_lighting;
 void main()
 {
 	gl_Position = vec4(vertex_position, 1.0f);
