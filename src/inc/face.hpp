c77023bc197d762dbf4265c3f4340471c008c96d
CobaltXII <sidatt64@gmail.com>
2018-12-28 21:56:21 -0500

    Implemented void load_block_face_info_array

--- src/inc/face.hpp
+++ src/inc/face.hpp
@@ -52,3 +52,99 @@ face_info* make_face_info_caps(float top, float bottom, float side)
 {
 	return new face_info(top, bottom, side, side, side, side);
 }
+
+// This function fills up block_face_info with a face_info* object for each
+// block_id. Call this function after calling load_block_texture_array, 
+// because this function uses block_name_to_layer as a lookup table.
+
+std::vector<face_info*> block_face_info;
+
+void load_block_face_info_array()
+{
+	block_face_info = 
+	{
+		make_face_info_all(-1.0f),
+
+		make_face_info_all(block_name_to_layer.at("bedrock")),
+
+		make_face_info_all(block_name_to_layer.at("water_0")),
+
+		make_face_info_all(block_name_to_layer.at("stone")),
+
+		make_face_info_all(block_name_to_layer.at("cobblestone")),
+
+		make_face_info_all(block_name_to_layer.at("dirt")),
+
+		make_face_info_caps(block_name_to_layer.at("grass_top"), block_name_to_layer.at("dirt"), block_name_to_layer.at("grass_side")),
+
+		make_face_info_caps(block_name_to_layer.at("mycelium_top"), block_name_to_layer.at("dirt"), block_name_to_layer.at("mycelium_side")),
+
+		make_face_info_all(block_name_to_layer.at("sand")),
+
+		make_face_info_all(block_name_to_layer.at("gravel")),
+
+		make_face_info_all(block_name_to_layer.at("brick")),
+
+		make_face_info_cap(block_name_to_layer.at("oak_planks"), block_name_to_layer.at("bookshelf")),
+
+		make_face_info_cap(block_name_to_layer.at("oak_log_top"), block_name_to_layer.at("oak_log_side")),
+
+		make_face_info_all(block_name_to_layer.at("oak_planks")),
+
+		make_face_info_all(block_name_to_layer.at("oak_leaves")),
+
+		make_face_info_cap(block_name_to_layer.at("birch_log_top"), block_name_to_layer.at("birch_log_side")),
+
+		make_face_info_all(block_name_to_layer.at("birch_planks")),
+
+		make_face_info_all(block_name_to_layer.at("birch_leaves")),
+
+		make_face_info_all(block_name_to_layer.at("mushroom_block_red")),
+
+		make_face_info_all(block_name_to_layer.at("mushroom_block_brown")),
+
+		make_face_info_cap(block_name_to_layer.at("mushroom_block_stem_top"), block_name_to_layer.at("mushroom_block_stem_side")),
+
+		make_face_info_all(block_name_to_layer.at("coal_ore")),
+
+		make_face_info_all(block_name_to_layer.at("iron_ore")),
+
+		make_face_info_all(block_name_to_layer.at("gold_ore")),
+
+		make_face_info_all(block_name_to_layer.at("diamond_ore")),
+
+		make_face_info_all(block_name_to_layer.at("emerald_ore")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_white")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_orange")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_magenta")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_light_blue")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_yellow")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_lime")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_pink")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_gray")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_silver")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_cyan")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_purple")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_blue")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_brown")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_green")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_red")),
+
+		make_face_info_all(block_name_to_layer.at("wool_colored_black"))
+	};
+}
