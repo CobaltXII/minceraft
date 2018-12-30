c491abf27867bfa2dccf0c7689354bb48163defe
CobaltXII <sidatt64@gmail.com>
2018-12-30 17:50:07 -0500

    Implemented inline void set_id_if_not_air

--- src/inc/world.hpp
+++ src/inc/world.hpp
@@ -115,6 +115,19 @@ struct world
 			voxel_set_id(voxels[x + x_res * (y + y_res * z)], id);
 		}
 	}
+
+	// Set the block_id information of the voxel at the specified coordinates,
+	// if the current block_id of the voxel at the specified coordinates is 
+	// not id_air.
+
+	inline void set_id_if_not_air(unsigned int x, unsigned int y, unsigned int z, block_id id)
+	{
+		if (get_id(x, y, z) != id_air)
+		{
+			voxel_set_id(voxels[x + x_res * (y + y_res * z)], id);
+		}
+	}
+
 	// Set the block_id information of the voxel at the specified coordinates,
 	// if the coordinates are within the bounds of the world.
 
