e6317720594d8660a33da61a1c0a9ad7af97dd46
CobaltXII <sidatt64@gmail.com>
2018-12-30 20:36:32 -0500

    All CMake related files are now visible

---
+++ bin/CMakeFiles/minceraft.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/src/glad/glad.c" "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/bin/CMakeFiles/minceraft.dir/src/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib"
+  "../src"
+  "../src/inc"
+  "/usr/local/Cellar/sdl2/2.0.9/include/SDL2"
+  "/usr/local/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/src/FastNoise/FastNoise.cpp" "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/bin/CMakeFiles/minceraft.dir/src/FastNoise/FastNoise.cpp.o"
+  "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/src/main.cpp" "/Users/cobalt/Desktop/Desktop - Adam’s iMac/Github/minceraft/bin/CMakeFiles/minceraft.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  "../src"
+  "../src/inc"
+  "/usr/local/Cellar/sdl2/2.0.9/include/SDL2"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/minceraft.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minceraft.dir/src/main.cpp.o"
+  "CMakeFiles/minceraft.dir/src/glad/glad.c.o"
+  "CMakeFiles/minceraft.dir/src/FastNoise/FastNoise.cpp.o"
+  "minceraft.pdb"
+  "minceraft"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/minceraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
