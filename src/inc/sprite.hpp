797be60ecbd8134590c788440aacfe31f5ec9d36
CobaltXII <sidatt64@gmail.com>
2019-01-03 16:07:12 -0500

    Implemented void load_sprite_preliminaries

--- src/inc/sprite.hpp
+++ src/inc/sprite.hpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+// you must call load_sprite_preliminaries() before doing any sprite based operations.
+
+GLuint sprite_shader_program;
+
+void load_sprite_preliminaries()
+{
+	sprite_shader_program = load_program("../glsl/sprite_vertex.glsl", "../glsl/sprite_fragment.glsl");
+}
 // A structure that represents a sprite loaded to the CPU.
 
 struct cpu_sprite
