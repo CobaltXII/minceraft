5bb28e6dd82d780a3d71580b0e4d2125aea73c46
CobaltXII <sidatt64@gmail.com>
2019-01-03 16:07:25 -0500

    Implemented cpu_sprite* allocate_sprite

--- src/inc/sprite.hpp
+++ src/inc/sprite.hpp
@@ -8,6 +8,7 @@ void load_sprite_preliminaries()
 {
 	sprite_shader_program = load_program("../glsl/sprite_vertex.glsl", "../glsl/sprite_fragment.glsl");
 }
+
 // A structure that represents a sprite loaded to the CPU.
 
 struct cpu_sprite
@@ -22,3 +23,32 @@ struct cpu_sprite
 
 	unsigned int offset;
 };
+
+// Allocate a chunk of memory (on the CPU) that can hold vertex_count vertices
+// with 2-dimensional coordinates and 2-dimensional texture coordinates.
+
+cpu_sprite* allocate_sprite(unsigned int vertex_count)
+{
+	float* memory = (float*)malloc(vertex_count * 4 * sizeof(float));
+
+	if (!memory)
+	{
+		std::cout << "Could not allocate a vertex array for " << vertex_count << " vertices." << std::endl;
+
+		exit(20);
+	}
+
+	cpu_sprite* the_cpu_sprite = new cpu_sprite();
+
+	the_cpu_sprite->vertices = memory;
+
+	the_cpu_sprite->size_in_bytes = vertex_count * 4 * sizeof(float);
+
+	the_cpu_sprite->size_in_floats = vertex_count * 4;
+
+	the_cpu_sprite->size_in_vertices = vertex_count;
+
+	the_cpu_sprite->offset = 0;
+
+	return the_cpu_sprite;
+}
