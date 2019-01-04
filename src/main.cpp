8bea3cf5ff5d35443654df1c903c6a556714c715
CobaltXII <sidatt64@gmail.com>
2019-01-04 16:06:48 -0500

    Implemented beetroot growth

--- src/main.cpp
+++ src/main.cpp
@@ -1033,6 +1033,29 @@ int main(int argc, char** argv)
 						the_growing_plant.done = true;
 					}
 				}
+				else if (the_growing_plant.type == growing_beetroots)
+				{
+					if (plant_id < id_beetroots_3)
+					{
+						the_accessor->set_id_safe
+						(
+							the_growing_plant.x,
+							the_growing_plant.y,
+							the_growing_plant.z,
+
+							block_id(plant_id + 1)
+						);
+
+						if (plant_id < id_beetroots_2)
+						{
+							the_growing_plant.timer = 60;
+						}
+					}
+					else
+					{
+						the_growing_plant.done = true;
+					}
+				}
 			}
 			else
 			{
