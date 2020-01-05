ba014268ca2b4f34151e9a915eb21a38618345c4
CobaltXII <sidatt64@gmail.com>
2020-01-05 15:44:41 -0500

    Added shaders for rendering text

---
+++ glsl/text_vertex.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+layout (location = 0) in vec2 vertex_position;
+layout (location = 1) in vec2 vertex_texture;
+layout (location = 2) in float vertex_lighting;
+
+out vec2 frag_texture;
+out float frag_lighting;
+
+void main() {
+	gl_Position = vec4(vertex_position, 0.0f, 1.0f);
+	frag_texture = vertex_texture;
+	frag_lighting = vertex_lighting;
+}
